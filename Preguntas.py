f42b81e577a7795bab3a6e4931087af756640313
FerDev03 <86682873+ferwis02@users.noreply.github.com>
2023-01-31 18:09:10 -0400

    Add files via upload

---
+++ Preguntas.py
@@ -0,0 +1,46 @@
+preguntas = [
+    ['hola', 'hey', 'saludos', 'klok', 'klk'],
+    ['orbi', 'notas', 'calificacion'],
+
+    ['inscripcion', 'reincripcion', 'monto'],
+
+    ['pago', 'caja', 'generales', 'pago en linea'],
+
+    ['Iniciar', 'Sesion', 'Iniciar sesion'],
+
+    ['pagina', 'seleccion', 'materias', 'cuando', 'fecha'],
+
+    ['pagina', 'seleccion', 'materias', 'donde', 'link'],
+
+    ['transporte', 'itla transporte', 'viaje', 'sistema'],
+
+    ['contacto', 'numero', 'correo', 'cual', 'itla'],
+
+    ['clase itla', 'teams', 'clases virtuales'],
+
+    ['muchas', 'gracias', 'adios', 'salir']
+]
+
+respuestas = [
+    'Buenos dias se esta comunicando con el ITLA que desea?',
+
+    'Para ver su calificacion o nota de su cuatrimestre porfavor ingresar con sus credenciales aqui: https://orbi.edu.do/orbi/gestiondocencia/grupo/calificacion',
+
+    'La reincripcion tiene un valor de $6,640.00 pesos',
+
+    'Para seleccionar su metodo de pago favor seguir los pasos de aqui: https://orbi.edu.do/orbi/servicio/pagoenlinea',
+
+    'Para iniciar sesion en el Orbi favor ingresar sus credenciales: orbi.edu.do',
+
+    'El dia de la seleccion de materias es enviado en el correo, favor estar pendiente a las fechas',
+
+    'La pagina de seleccion de materias es orbi.do la cual esta habilitadas las fechas que ve en el correo enviado sobre dicha info',
+
+    'Para proceder al sistema de transporte iniciar sesion aca: https://transporte.itla.edu.do/transporte/login',
+
+    'Los numeros de contacto del ITLA los puede ver aqui: https://itla.edu.do/contactos/',
+
+    'Las clases del itla ahora estan siendo impartidas por medio de teams educacion.',
+
+    'Fue un placer ayudarle'
+]
